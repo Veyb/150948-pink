4ecf9aa9e0c944091abc883d68fddc77e4818eb5
Veyb <veymovie@gmail.com>
2016-03-27 21:14:54 +0300

    fix gulpfile, fix img url, fix flex in form.html

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,7 @@ gulp.task("style", function() {
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 1 version",
-        "last 2 Chrome versions",
-        "last 2 Firefox versions",
-        "last 2 Opera versions",
-        "last 2 Edge versions"
-      ]})
+      autoprefixer()
     ]))
     .pipe(gulp.dest("css"))
     .pipe(server.reload({stream: true}));
